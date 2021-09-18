64864a609f7189ceb1e1cb61d6c197c254e65a31
Luiz Cavalieri <luiz@interactivehippo.com.au>
2021-09-18 15:10:52 +1000

    Setup prod build in webpack

---
+++ container/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const { merge } = require('webpack-merge')
+const commonConfig = require('./webpack.common')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const packageJson = require('../package.json')
+
+const { PRODUCTION_DOMAIN } = process.env
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'containerModule',
+      remotes: {
+        marketingApp: `marketingModule@${PRODUCTION_DOMAIN}/marketing/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies,
+    }),
+  ]
+}
+
+module.exports = merge(commonConfig, prodConfig)
---
+++ marketing/config/webpack.prod.js
@@ -0,0 +1,23 @@
+const { merge } = require('webpack-merge')
+const commonConfig = require('./webpack.common')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const packageJson = require('../package.json')
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      exposes: {
+        './MarketingApp': './src/bootstrap'
+      },
+      filename: 'remoteEntry.js',
+      name: 'marketingModule',
+      shared: packageJson.dependencies,
+    }),
+  ]
+}
+
+module.exports = merge(commonConfig, prodConfig)
