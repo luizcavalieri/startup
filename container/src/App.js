074a36b0ea1d385274efa30816ef37d5808febc3
Luiz Cavalieri <luiz@interactivehippo.com.au>
2021-09-26 15:17:00 +1000

    Add header

--- container/src/App.js
+++ container/src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { mount } from 'marketingApp/MarketingApp'
 import AppRenderer from './components/AppRenderer'
+import Header from './components/Header'
 
 const MarketingApp = () => <AppRenderer mount={mount} />
 
@@ -10,6 +11,7 @@ export default () => {
   return (
     <StylesProvider>
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route component={MarketingApp} exact path="/" />
         </Switch>
