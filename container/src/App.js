f4f2e0e397ab439ac5fb5495829ef587a080f56a
Luiz Cavalieri <luiz@interactivehippo.com.au>
2021-09-26 15:35:41 +1000

    Add generateClassName to top level styles

--- container/src/App.js
+++ container/src/App.js
@@ -1,15 +1,17 @@
-import { StylesProvider } from '@material-ui/core'
 import React from 'react'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { mount } from 'marketingApp/MarketingApp'
 import AppRenderer from './components/AppRenderer'
 import Header from './components/Header'
 
+const generateClassName = createGenerateClassName({ productionPrefix: 'cont' })
+
 const MarketingApp = () => <AppRenderer mount={mount} />
 
 export default () => {
   return (
-    <StylesProvider>
+    <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <Header />
         <Switch>
