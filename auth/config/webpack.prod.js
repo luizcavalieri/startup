2cb5f36310516e2dec6bcfe16893a428cf22d9bd
Luiz Cavalieri <luiz@interactivehippo.com.au>
2021-10-04 10:55:26 +1100

    Bootstrap auth app

---
+++ auth/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const { merge } = require('webpack-merge')
+const commonConfig = require('./webpack.common')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const packageJson = require('../package.json')
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    publicPath: '/marketing/latest/',
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      exposes: {
+        './AuthApp': './src/bootstrap'
+      },
+      filename: 'remoteEntry.js',
+      name: 'authModule',
+      shared: packageJson.dependencies,
+    }),
+  ]
+}
+
+module.exports = merge(commonConfig, prodConfig)
---
+++ auth/src/bootstrap.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createBrowserHistory, createMemoryHistory } from 'history'
+import App from './App'
+
+const mount = (el, { defaultHistory, onNavigate }) => {
+  const history = defaultHistory || createMemoryHistory()
+  if (onNavigate) history.listen(onNavigate)
+
+  ReactDOM.render(<App history={history} />, el)
+
+  return {
+    onParentNavigate: ({ pathname: nextPathname }) => {
+      const { location, push } = history
+      if (location.pathname !== nextPathname) {
+        push(nextPathname)
+      }
+    }
+  }
+}
+
+if (process.env.NODE_ENV === 'development') {
+  const app = document.getElementById('app')
+  if (app) {
+    mount(app, { defaultHistory: createBrowserHistory(), onNavigate: () => undefined })
+  }
+}
+
+export { mount }
